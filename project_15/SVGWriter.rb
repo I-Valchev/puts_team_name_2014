6cff9f19ee14e22694e65acf4e41d991b7d7875c
dimitur.trz <dimitar.trz@gmail.com>
2014-11-13 21:55:34 +0200

    svg-update

--- project_15/SVGWriter.rb
+++ project_15/SVGWriter.rb
@@ -4,33 +4,34 @@ class SVGWriter
 	def write data, time_taken
 
 		valid_tasks = Array.new(10)
-		valid_tasks[0] = 0
-		valid_tasks[1] = 0
-		valid_tasks[2] = 0
-		valid_tasks[3] = 0
-		valid_tasks[4] = 0
-		valid_tasks[5] = 0
-		valid_tasks[6] = 0
+		0.upto(6) do |init_ind|
+			valid_tasks[init_ind] = 0
+		end
 
 		data.each do |value, i|
-			valid_tasks[0] += 1 if i[0].to_i == 2
-			valid_tasks[1] += 1 if i[1].to_i == 2
-			valid_tasks[2] += 1 if i[2].to_i == 2
-			valid_tasks[3] += 1 if i[3].to_i == 2
-			valid_tasks[4] += 1 if i[4].to_i == 2
-			valid_tasks[5] += 1 if i[5].to_i == 2
-			valid_tasks[6] += 1 if i[6].to_i == 2
+			0.upto(6){|checked_index|
+				valid_tasks[checked_index] += 1 if i[checked_index].to_i == 2
+			}
 		end
 
 		b = Nokogiri::XML::Builder.new do |doc|
-		  doc.svg xmlns:"http://www.w3.org/2000/svg", viewBox:"0 -150 200 200" do
-		  	0.upto(6){|column|
-		  		doc.rect x:column*15, y:-valid_tasks[column], width:10, height: valid_tasks[column]
-		  		doc.text_ "text"
-		  	}
-		      	      #doc.text(x:80, y:0){"text"}
+		  doc.svg xmlns:"http://www.w3.org/2000/svg", viewBox:"0 -400 500 500" do
+		  	column_separation = 15
+		  	0.upto(6) do |column|
+		  		percentage = (valid_tasks[column]/58.0)*100
+		  		doc.rect x:(column*column_separation*5), y:-valid_tasks[column]*5, width:50, height: valid_tasks[column]*5, stroke:"red"
+				doc.text_ "#{percentage.round(2)}%", x:(column*75), y:-(valid_tasks[column]*5)-10#, font-size:24
+				doc.text_ "vh_nivo", x:(column*column_separation*5), y:+column_separation if column==0
+				doc.text_ "class002", x:(column*column_separation*5), y:+column_separation if column==1
+				doc.text_ "class003", x:(column*column_separation*5), y:+column_separation if column==2
+				doc.text_ "class004", x:(column*column_separation*5), y:+column_separation if column==3
+				doc.text_ "class009", x:(column*column_separation*5), y:+column_separation if column==4
+				doc.text_ "class012", x:(column*column_separation*5), y:+column_separation if column==5
+				doc.text_ "class014", x:(column*column_separation*5), y:+column_separation if column==6
+		  	end
 		  end
 		end
+		puts b.to_xml
 		File.open("results_Dimitar_Terziev_A_6.svg", "w") do |file|
 			file.puts b.to_xml
 		end
