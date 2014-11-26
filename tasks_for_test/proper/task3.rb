4389ffc2ea802a594f6d024c09b33a51148c0e65
dimitur.trz <dimitar.trz@gmail.com>
2014-11-26 18:47:56 +0200

    task3

--- tasks_for_test/proper/task3.rb
+++ tasks_for_test/proper/task3.rb
@@ -11,17 +11,19 @@ class Task3 < Task
 			:expected=><<-EOS
 {
   "students_with_zero": [
-    "Dimitar_Andreev",
-    "Dimitar_Matev",
-    "Dimitar_Nestorov",
-    "Georgi_Ivanov",
-    "Kaloyan_Nikov",
-    "Konstantin_Kostov",
-    "Marian_Belchev",
-    "Simeon_Shopkin",
-    "Stanislav_Gospodinov",
-    "atanaska_ivancheva",
-    "mladen_karadimov"
+   	"student":[
+   		"Dimitar_Andreev",
+		"Dimitar_Matev",
+		"Dimitar_Nestorov",
+		"Georgi_Ivanov",
+		"Kaloyan_Nikov",
+		"Konstantin_Kostov",
+		"Marian_Belchev",
+		"Simeon_Shopkin",
+		"Stanislav_Gospodinov",
+		"atanaska_ivancheva",
+		"mladen_karadimov"
+   	]
   ]
 }
 EOS
@@ -34,17 +36,19 @@ EOS
 			:expected=><<-EOS
 {
   "students_with_zero": [
-    "Dimitar_Andreev",
-    "Marian_Belchev",
-    "Stanislav_Gospodinov",
-    "Georgi_Ivanov",
-    "Konstantin_Kostov",
-    "Dimitar_Matev",
-    "Dimitar_Nestorov",
-    "Kaloyan_Nikov",
-    "Simeon_Shopkin",
-    "atanaska_ivancheva",
-    "mladen_karadimov"
+  	"student":[
+		"Dimitar_Andreev",
+		"Marian_Belchev",
+		"Stanislav_Gospodinov",
+		"Georgi_Ivanov",
+		"Konstantin_Kostov",
+		"Dimitar_Matev",
+		"Dimitar_Nestorov",
+		"Kaloyan_Nikov",
+		"Simeon_Shopkin",
+		"atanaska_ivancheva",
+		"mladen_karadimov"
+    ]
   ]
 }
 EOS
@@ -57,17 +61,19 @@ EOS
 			:expected=><<-EOS
 {
   "students_with_zero": [
-    "mladen_karadimov",
-    "atanaska_ivancheva",
-    "Simeon_Shopkin",
-    "Kaloyan_Nikov",
-    "Dimitar_Nestorov",
-    "Dimitar_Matev",
-    "Konstantin_Kostov",
-    "Georgi_Ivanov",
-    "Stanislav_Gospodinov",
-    "Marian_Belchev",
-    "Dimitar_Andreev"
+  	"student":[
+		"mladen_karadimov",
+		"atanaska_ivancheva",
+		"Simeon_Shopkin",
+		"Kaloyan_Nikov",
+		"Dimitar_Nestorov",
+		"Dimitar_Matev",
+		"Konstantin_Kostov",
+		"Georgi_Ivanov",
+		"Stanislav_Gospodinov",
+		"Marian_Belchev",
+		"Dimitar_Andreev"
+    ]
   ]
 }
 EOS
@@ -80,17 +86,19 @@ EOS
 			:expected=><<-EOS
 {
   "students_with_zero": [
-    "mladen_karadimov",
-    "atanaska_ivancheva",
-    "Stanislav_Gospodinov",
-    "Simeon_Shopkin",
-    "Marian_Belchev",
-    "Konstantin_Kostov",
-    "Kaloyan_Nikov",
-    "Georgi_Ivanov",
-    "Dimitar_Andreev",
-    "Dimitar_Matev",
-    "Dimitar_Nestorov"
+  	"student":[
+	    "mladen_karadimov",
+		"atanaska_ivancheva",
+		"Stanislav_Gospodinov",
+		"Simeon_Shopkin",
+		"Marian_Belchev",
+		"Konstantin_Kostov",
+		"Kaloyan_Nikov",
+		"Georgi_Ivanov",
+		"Dimitar_Andreev",
+		"Dimitar_Matev",
+		"Dimitar_Nestorov"
+    ]
   ]
 }
 EOS
