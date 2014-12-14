ff0047458e2c0444d0181b91d9d22cc0ccf8df55
stamaniorec <stamaniorec@gmail.com>
2014-12-14 22:10:04 +0200

    No need to thank me.

---
+++ oop_ftw_mess/Stanimir_Bogdanov_A_24.rb
@@ -0,0 +1,19 @@
+require 'time'
+require_relative 'HomeworkChecker'
+require_relative 'CommandLineArgumentsParser'
+
+start_time = Time.now
+
+command_line_arguments_parser = CommandLineArgumentsParser.new(ARGV)
+output_file_format = command_line_arguments_parser.get_output_file_format(default: 'csv') 
+how_many_to_check = command_line_arguments_parser.get_number_of_programs_to_check 
+repo_folder = command_line_arguments_parser.get_repo_folder
+
+require_relative "#{output_file_format.upcase}Writer"
+
+checker = HomeworkChecker.new(repo_folder, how_many_to_check)
+checker.check_homeworks
+
+writer = eval("#{output_file_format.upcase}Writer.new checker")
+
+writer.write(checker.data.sort, Time.now - start_time)
